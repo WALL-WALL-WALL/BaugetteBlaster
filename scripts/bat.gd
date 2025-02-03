df7fa42af257673e8f2caadaf6b68f9e0466b31d
WALL-WALL-WALL <wswaetje@gmail.com>
2025-02-02 19:55:21 -0700

    alters

---
+++ scripts/bat.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+var direction = 1
+var flipped = false
+
+
+func _process(_delta):
+	# Handle jump.
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+
+	if direction > 0:
+		$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.flip_h = true
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	$AnimatedSprite2D.play("default")
+	if flipped == false:
+		flipped = true
+		$flip.start()
+	move_and_slide()
+
+
+
+func _on_flip_timeout() -> void:
+	direction = direction * -1
+	flipped = false
